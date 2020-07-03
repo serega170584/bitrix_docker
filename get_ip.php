1564144b0f92c757b5b69e37b9e6c79c90a79d30
krivobokov.sv <krivobokov.sv@academia-moscow.ru>
2020-07-03 09:47:37 +0300

    add get ip script

--- get_ip.php
+++ get_ip.php
@@ -2,5 +2,5 @@
 error_reporting(E_ALL & ~E_DEPRECATED);
 //$fdescr = fopen('php://stdin', 'r');
 $str = file_get_contents('php://stdin');
-var_dump($str);
-//echo json_decode($str);
+//var_dump($str);
+var_dump(json_decode($str));
