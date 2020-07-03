aa427bec7874515663082a0a7ecf583860964970
krivobokov.sv <krivobokov.sv@academia-moscow.ru>
2020-07-03 16:09:54 +0300

    get ip fix

--- get_ip.php
+++ get_ip.php
@@ -1,3 +1,4 @@
 <?php
+error_reporting(E_ALL | ~E_DEPRECATED);
 $str = file_get_contents('php://stdin');
-var_dump(json_decode($str)[0]->NetworkSettings->Networks->bitrix_docker_default);
+var_dump(json_decode($str)[0]->NetworkSettings->Networks->bitrix_docker_default->IPAddress);
