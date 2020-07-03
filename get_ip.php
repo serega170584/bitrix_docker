46e96f8d9aa5f3ca5afdd0c1f3a4c0b218cfd61b
krivobokov.sv <krivobokov.sv@academia-moscow.ru>
2020-07-03 16:13:00 +0300

    get ip fix

--- get_ip.php
+++ get_ip.php
@@ -1,4 +1,7 @@
 <?php
-error_reporting(0);
 $str = file_get_contents('php://stdin');
-var_dump(json_decode($str)[0]->NetworkSettings->Networks->bitrix_docker_default->IPAddress);
+try {
+    var_dump(json_decode($str)[0]->NetworkSettings->Networks->bitrix_docker_default->IPAddress);
+} catch (Exception $e) {
+    echo 'error';
+}
