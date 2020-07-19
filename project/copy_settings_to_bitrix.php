35523ec253d56d0d33fff9f06529dd77bcebe348
krivobokov.sv <krivobokov.sv@academia-moscow.ru>
2020-07-19 12:49:13 +0300

    copy settings to bitrix

--- project/copy_settings_to_bitrix.php
+++ project/copy_settings_to_bitrix.php
@@ -1,2 +1,2 @@
 <?php
-var_dump(copy($_SERVER['DOCUMENT_ROOT'].'/project/settings.php'),$_SERVER['DOCUMENT_ROOT'].'/bitrix/.settings.php');
+var_dump(copy($_SERVER['DOCUMENT_ROOT'].'/project/settings.php',$_SERVER['DOCUMENT_ROOT'].'/bitrix/.settings.php'));
