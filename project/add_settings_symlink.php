f8e6423aaa3f71523cec07114a1b7346c5ccf84b
krivobokov.sv <krivobokov.sv@academia-moscow.ru>
2020-07-19 12:18:39 +0300

    add settings symlink

--- project/add_settings_symlink.php
+++ project/add_settings_symlink.php
@@ -1,2 +1,2 @@
 <?php
-symlink($_SERVER['DOCUMENT_ROOT'].'/bitrix/.settings.php', $_SERVER['DOCUMENT_ROOT'].'/settings.php');
+var_dump(symlink($_SERVER['DOCUMENT_ROOT'].'/bitrix/.settings.php', $_SERVER['DOCUMENT_ROOT'].'/settings.php'));
